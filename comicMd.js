fdada25ca9711f0cf9629b45b32415257c7cc5a0
mio <10892119+biuuu@users.noreply.github.com>
2021-07-02 21:42:04 +0800

    add pages

--- comicMd.js
+++ comicMd.js
@@ -1,18 +1,53 @@
 const data = require('./dist/4ko.json')
 const fs = require('fs-extra')
 
-data.forEach(item => {
-  const key = `${item[0]}`
-  const { title, name } = item[1]
+const getHeader = (data) => {
+  const key = `${data[0]}`
+  const { title, name } = data[1]
   let header = ''
   if (key.startsWith('1')) {
-    header = `第${item[0] - 10000}话 ${title}`
+    header = `第${data[0] - 10000}话 ${title}`
   } else if (key.startsWith('2')) {
-    header = `第${item[0] - 20000}话 ${title}`
+    header = `第${data[0] - 20000}话 ${title}`
   } else if (key.startsWith('3')) {
-    header = `第${item[0] - 30000}话 ${title}`
+    header = `第${data[0] - 30000}话 ${title}`
   }
-  const content = `### ${header}
+  return [header, key]
+}
+
+let prev = ''
+let next = ''
+data.forEach((item, index) => {
+  const key = `${item[0]}`
+  const { title, name } = item[1]
+  let header = getHeader(item)
+
+  if (data[index - 1]) {
+    let [text, link] = getHeader(data[index - 1])
+    prev = `prev:
+  text: ${text}
+  link: /4ko/${link}.html`
+  } else {
+    prev = ''
+  }
+
+  if (data[index + 1]) {
+    let [text, link] = getHeader(data[index + 1])
+    next = `next:
+  text: ${text}
+  link: /4ko/${link}.html`
+  } else {
+    next = ''
+  }
+
+
+  const content = `---
+${prev}
+${next}
+---
+<h1 style="text-align:center">${header[0]}</h1>
+
 ![](/4ko/${name})`
-  fs.outputFileSync(`./pages/${key}.md`, content)
+  fs.outputFileSync(`./pages/4ko/${key}.md`, content)
+  fs.copySync('./dist/4ko.json', './pages/.vuepress/public/4ko.json')
 })
--- pages/.vuepress/config.js
+++ pages/.vuepress/config.js
@@ -1,18 +1,18 @@
 const data = require('./public/4ko.json')
 
 const web4ko = {
-  text: 'WEB 4格漫画',
-  link: '/web4ko',
+  text: 'WEB 四格漫画',
+  link: '/4ko/10000.md',
   children: []
 }
 const special4ko = {
-  text: '特别篇 4格漫画',
-  link: '/special',
+  text: '特别篇 四格漫画',
+  link: '/4ko/20001.md',
   children: []
 }
 const limit4ko = {
-  text: '限定 4格漫画',
-  link: '/limit',
+  text: '限定 四格漫画',
+  link: '/4ko/30001.md',
   children: []
 }
 data.forEach(item => {
@@ -21,19 +21,19 @@ data.forEach(item => {
   if (key.startsWith('1')) {
     web4ko.children.push({
       text: `第${item[0] - 10000}话 ${title}`,
-      link: `/${key}.md`
+      link: `/4ko/${key}.md`
     })
   }
   if (key.startsWith('2')) {
     special4ko.children.push({
       text: `第${item[0] - 20000}话 ${title}`,
-      link: `/${key}.md`
+      link: `/4ko/${key}.md`
     })
   }
   if (key.startsWith('3')) {
     limit4ko.children.push({
       text: `第${item[0] - 30000}话 ${title}`,
-      link: `/${key}.md`
+      link: `/4ko/${key}.md`
     })
   }
 })
@@ -41,12 +41,34 @@ module.exports = {
   lang: 'zh-CN',
   title: '闪耀色彩 四格漫画',
   description: '偶像大师闪耀色彩的四格漫画汉化版',
-
+  head: [['link', { rel: 'icon', href: '/icon.png' }]],
   themeConfig: {
+    logo: '/icon.png',
+    repo: 'https://github.com/ShinyGroup/Comics',
+    editLink: false,
+    lastUpdated: false,
+    contributors: false,
     sidebar: [
+      {
+        text: '前言',
+        link: '/README.md',
+        children: [
+          {
+            text: '简介',
+            link: '/README.md#简介'
+          },
+          {
+            text: '关于',
+            link: '/README.md#关于'
+          }
+        ]
+      },
       web4ko,
       special4ko,
       limit4ko
     ],
   },
+  plugins: [
+
+  ]
 }
