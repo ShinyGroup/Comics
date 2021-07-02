74fef2024a2bc4d3a9c515cc5fc9467ce6c98322
mio <10892119+biuuu@users.noreply.github.com>
2021-07-03 01:45:36 +0800

    update

--- pages/.vuepress/config.js
+++ pages/.vuepress/config.js
@@ -1,19 +1,23 @@
 const data = require('./public/4ko.json')
 
 const web4ko = {
-  title: 'WEB 四格漫画',
+  text: 'WEB 四格漫画',
+  link: '/4ko/10000.md',
   children: []
 }
 const special4ko = {
-  title: '特别篇 四格漫画',
+  text: '特别篇 四格漫画',
+  link: '/4ko/20001.md',
   children: []
 }
 const limit4ko = {
-  title: '限定 四格漫画',
+  text: '限定 四格漫画',
+  link: '/4ko/30001.md',
   children: []
 }
 const musicDawn4ko = {
-  title: 'MusicDawn 四格漫画',
+  text: 'MusicDawn 四格漫画',
+  link: '/4ko/90001.md',
   children: []
 }
 data.forEach(item => {
@@ -21,23 +25,23 @@ data.forEach(item => {
   const { title, name } = item[1]
   if (key.startsWith('9')) {
     musicDawn4ko.children.push({
-      title: `第${item[0] - 90000}话 ${title}`,
-      path: `/4ko/${key}.md`
+      text: `第${item[0] - 90000}话 ${title}`,
+      link: `/4ko/${key}.md`
     })
   } else if (key.startsWith('1')) {
     web4ko.children.push({
-      title: `第${item[0] - 10000}话 ${title}`,
-      path: `/4ko/${key}.md`
+      text: `第${item[0] - 10000}话 ${title}`,
+      link: `/4ko/${key}.md`
     })
   } else if (key.startsWith('2')) {
     special4ko.children.push({
-      title: `第${item[0] - 20000}话 ${title}`,
-      path: `/4ko/${key}.md`
+      text: `第${item[0] - 20000}话 ${title}`,
+      link: `/4ko/${key}.md`
     })
   } else if (key.startsWith('3')) {
     limit4ko.children.push({
-      title: `第${item[0] - 30000}话 ${title}`,
-      path: `/4ko/${key}.md`
+      text: `第${item[0] - 30000}话 ${title}`,
+      link: `/4ko/${key}.md`
     })
   }
 })
@@ -45,7 +49,15 @@ module.exports = {
   lang: 'zh-CN',
   title: '闪耀色彩 四格漫画',
   description: '偶像大师闪耀色彩的四格漫画汉化版',
-  head: [['link', { rel: 'icon', href: '/icon.png' }]],
+  head: [
+    ['link', { rel: 'icon', href: '/icon.png' }],
+    ['style', { type: 'text/css' }, `.sidebar-heading+ul {
+      display: none;
+    }
+    .sidebar-heading.active+ul {
+      display: block;
+    }`]
+  ],
   themeConfig: {
     logo: '/icon.png',
     repo: 'https://github.com/ShinyGroup/Comics',
@@ -53,10 +65,10 @@ module.exports = {
     lastUpdated: false,
     contributors: false,
     search: false,
-    nav: [
+    navbar: [
       {
         text: '汉化插件',
-        items: [
+        children: [
           {
             text: '使用指南',
             link: 'https://github.com/biuuu/ShinyColors/blob/master/src/README.md'
@@ -70,8 +82,8 @@ module.exports = {
     ],
     sidebar: [
       {
-        title: '前言',
-        path: '/',
+        text: '前言',
+        link: '/'
 
       },
       web4ko,
